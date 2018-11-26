d7217772717c7627589db37a24397019985f3faa
chexueji <chexueji@163.com>
2018-11-26 13:04:52 +0800

    fix bug

--- WebGL Project Template/template.js
+++ WebGL Project Template/template.js
@@ -1,111 +1,64 @@
 
 // Vertex shader program
-var VSHADER_SOURCE = null;
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Color;\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '  v_Color = a_Color;\n' +
+  '}\n';
+
 // Fragment shader program
-var FSHADER_SOURCE = null;
+var FSHADER_SOURCE =
+  '#ifdef GL_ES\n' +
+  'precision mediump float;\n' +
+  '#endif\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_FragColor = v_Color;\n' +
+  '}\n';
 
 function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
+  var canvas = document.getElementById('webgl'); // Retrieve <canvas> element
+  var gl = getWebGLContext(canvas);              // Get the rendering context for WebGL
+  initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);   // Initialize shaders
+  var n = initVertexBuffers(gl);      // Set vertex coordinates and colors
 
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
-  // Read shader from file
-  readShaderFile(gl, 'ColoredTriangle.vert', 'v');
-  readShaderFile(gl, 'ColoredTriangle.frag', 'f');
-}
-
-function start(gl) {
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // Set vertex information
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log('Failed to set the vertex information');
-    return;
-  }
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Draw the rectangle
-  gl.drawArrays(gl.TRIANGLES, 0, n);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Specify the color for clearing <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);      // Clear <canvas>
+  gl.drawArrays(gl.TRIANGLES, 0, n);  // Draw the triangles
 }
 
 function initVertexBuffers(gl) {
-  var verticesColors = new Float32Array([
-    // Vertex coordinates and color
-     0.0,  0.5,  1.0,  0.0,  0.0, 
-    -0.5, -0.5,  0.0,  1.0,  0.0, 
-     0.5, -0.5,  0.0,  0.0,  1.0, 
+  var pc = new Float32Array([ // Vertex coordinates and color
+     0.0,  0.5,  -0.1,  0.0,  0.0,  1.0,  // The front blue one 
+    -0.5, -0.5,  -0.1,  0.0,  0.0,  1.0,
+     0.5, -0.5,  -0.1,  1.0,  1.0,  0.0, 
+
+     0.5,  0.4,  -0.5,  1.0,  1.0,  0.0,  // The red triangle is behind
+    -0.5,  0.4,  -0.5,  1.0,  0.0,  0.0,
+     0.0, -0.6,  -0.5,  1.0,  0.0,  0.0, 
   ]);
-  var n = 3;
+  var numVertex = 3; var numColor = 3; var n = 6;
 
-  // Create a buffer object
-  var vertexColorBuffer = gl.createBuffer();  
-  if (!vertexColorBuffer) {
-    console.log('Failed to create the buffer object');
-    return false;
-  }
+  // Create a buffer object and write data to it
+  var pcbuffer = gl.createBuffer();  
+  gl.bindBuffer(gl.ARRAY_BUFFER, pcbuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, pc, gl.STATIC_DRAW);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+  var FSIZE = pc.BYTES_PER_ELEMENT;   // The number of byte
+  var STRIDE = numVertex + numColor;　// Stride
 
-  var FSIZE = verticesColors.BYTES_PER_ELEMENT;
-  //Get the storage location of a_Position, assign and enable buffer
+  // Assign the vertex coordinates to attribute variable and enable the assignment
   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return -1;
-  }
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
-  gl.enableVertexAttribArray(a_Position);  // Enable the assignment of the buffer object
+  gl.vertexAttribPointer(a_Position, numVertex, gl.FLOAT, false, FSIZE * STRIDE, 0);
+  gl.enableVertexAttribArray(a_Position);
 
-  // Get the storage location of a_Position, assign buffer and enable
+  // Assign the vertex colors to attribute variable and enable the assignment
   var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-  if(a_Color < 0) {
-    console.log('Failed to get the storage location of a_Color');
-    return -1;
-  }
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
-  gl.enableVertexAttribArray(a_Color);  // Enable the assignment of the buffer object
+  gl.vertexAttribPointer(a_Color, numColor, gl.FLOAT, false, FSIZE * STRIDE, FSIZE * numVertex);
+  gl.enableVertexAttribArray(a_Color);
 
   return n;
 }
- 
-// Read shader from file
-function readShaderFile(gl, fileName, shader) {
-  var request = new XMLHttpRequest();
-
-  request.onreadystatechange = function() {
-    if (request.readyState === 4 && request.status !== 404) { 
-	onReadShader(gl, request.responseText, shader); 
-    }
-  }
-  request.open('GET', fileName, true); // Create a request to acquire the file
-  request.send();                      // Send the request
-}
-
-// The shader is loaded from file
-function onReadShader(gl, fileString, shader) {
-  if (shader == 'v') { // Vertex shader
-    VSHADER_SOURCE = fileString;
-  } else 
-  if (shader == 'f') { // Fragment shader
-    FSHADER_SOURCE = fileString;
-  }
-  // When both are available, call start().
-  if (VSHADER_SOURCE && FSHADER_SOURCE) start(gl);
-}
-
